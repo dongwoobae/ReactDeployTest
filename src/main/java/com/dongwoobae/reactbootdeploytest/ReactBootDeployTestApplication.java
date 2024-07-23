216c152245742417f58245b7cd0fe10a736d2d52
dongwoobae <https://github.com/dongwoobae/naver0314devops11.git>
2024-07-23 16:59:54 +0900

    final-first

---
+++ src/main/java/com/dongwoobae/reactbootdeploytest/ReactBootDeployTestApplication.java
@@ -0,0 +1,13 @@
+package com.dongwoobae.reactbootdeploytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactBootDeployTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactBootDeployTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dongwoobae/reactbootdeploytest/ReactBootDeployTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongwoobae.reactbootdeploytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactBootDeployTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
